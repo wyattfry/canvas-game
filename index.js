4b2372f343966806c0ad13f574d7a6fe5ceb012c
Wyatt Fry <wyattfry@gmail.com>
2022-08-10 21:13:37 -0400

    fix rank

--- index.js
+++ index.js
@@ -25,31 +25,6 @@ function init() {
     particles = []
 }
 
-
-async function updateLeaderboard() {
-    const leaderboardBody = document.querySelector("#leaderboardBody")
-    const response = await fetch('/scores')
-    const scores = await response.json()
-    let rank = 10
-    leaderboardBody.innerHTML = ''
-    scores.forEach(record => {
-        const row = document.createElement('tr')
-
-        const rankCell = document.createElement('td')
-        rankCell.innerText = rank--
-
-        const nameCell = document.createElement('td')
-        nameCell.innerText = record[0]
-
-        const scoreCell = document.createElement('td')
-        scoreCell.innerText = record[1]
-
-        row.append(rankCell, nameCell, scoreCell)
-
-        leaderboardBody.append(row)
-    })
-}
-
 let mouseY
 let mouseX
 
@@ -71,7 +46,6 @@ function shootHandler(e) {
 // ##  On click shoot projectile
 window.addEventListener('click', shootHandler)
 window.addEventListener('keyup', event => {
-    console.log('key press')
     if (event.code === 'Space') {
         shootHandler()
     }
@@ -154,6 +128,7 @@ function animate() {
             while (playerName.length < 1 || playerName.length > 3) {
                 playerName = prompt(`Enter your name (three characters max)`)
             }
+            // record score
             fetch(`/scores?name=${playerName}&score=${getScore()}`, {method: 'POST'})
                 .then(updateLeaderboard)
                 .then(showModal)
