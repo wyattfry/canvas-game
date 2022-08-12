f446284aef536ba38abf6cd97d34e1029d23e99e
Wyatt Fry <wyattfry@gmail.com>
2022-08-12 11:50:14 -0400

    forgot to include these files

---
+++ version.js
@@ -0,0 +1,5 @@
+async function getVersion() {
+    const response = await fetch("/version")
+    const version = await response.text()
+    document.querySelector("#versionEl").innerText = version
+}
