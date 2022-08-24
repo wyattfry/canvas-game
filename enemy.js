4bac2ff5bc74f2dedd0003bf1bbb605e629de6c3
Wyatt Fry <wyattfry@gmail.com>
2022-08-24 17:49:07 -0400

    restore versions from main

--- enemy.js
+++ enemy.js
@@ -71,9 +71,7 @@ function spawnEnemies(cancellationToken) {
       };
       const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
       enemies.push(new Enemy(enemyX, enemyY, radius, color, velocity));
-      spawnIntervalMs = spawnIntervalMs > 500
-        ? Math.floor(spawnIntervalMs * 0.95)
-        : spawnIntervalMs;
+      spawnIntervalMs = spawnIntervalMs > 500 ? Math.floor(spawnIntervalMs * 0.95) : spawnIntervalMs;
       setTimeout(() => spawnEnemies(cancellationToken), spawnIntervalMs);
     }, spawnIntervalMs);
   }
